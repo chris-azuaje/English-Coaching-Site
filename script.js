ed7b3d823b8407a7b983c977e7f46cce67990aa0
Chris Azuaje <christopherazuaje@gmail.com>
2024-03-27 14:13:59 -0400

    Drop down text functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqQuestions = document.querySelectorAll(".question-container");
+
+  faqQuestions.forEach((question) => {
+    question.addEventListener("click", () => {
+      const answer = question.nextElementSibling;
+      const arrow = document.querySelectorAll(".arrow");
+
+      answer.classList.toggle("active");
+
+      //   arrow.forEach((item) => item.classList.add("arrow-down"));
+      arrow.forEach((arrow) =>
+        arrow.classList.contains("arrow-down")
+          ? arrow.classList.remove("arrow-down")
+          : arrow.classList.add("arrow-down")
+      );
+
+      document.querySelectorAll(".answer-container").forEach((otherAnswer) => {
+        if (otherAnswer !== answer) {
+          otherAnswer.classList.remove("active");
+        }
+      });
+    });
+  });
+});
