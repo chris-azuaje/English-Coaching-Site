5ccb9aacf8fe7762c0749f31bb6313b4dfaa11ce
Chris Azuaje <christopherazuaje@gmail.com>
2024-04-01 22:57:23 -0400

    changes

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const faqQuestions = document.querySelectorAll(".question-container");
+document.addEventListener('DOMContentLoaded', () => {
+  const faqQuestions = document.querySelectorAll('.question-container');
 
   faqQuestions.forEach((question) => {
-    question.addEventListener("click", () => {
+    question.addEventListener('click', () => {
       const answer = question.nextElementSibling;
-      const arrow = document.querySelectorAll(".arrow");
+      answer.classList.toggle('active');
 
-      answer.classList.toggle("active");
-
-      //   arrow.forEach((item) => item.classList.add("arrow-down"));
-      arrow.forEach((arrow) =>
-        arrow.classList.contains("arrow-down")
-          ? arrow.classList.remove("arrow-down")
-          : arrow.classList.add("arrow-down")
-      );
-
-      document.querySelectorAll(".answer-container").forEach((otherAnswer) => {
+      document.querySelectorAll('.answer-container').forEach((otherAnswer) => {
         if (otherAnswer !== answer) {
-          otherAnswer.classList.remove("active");
+          otherAnswer.classList.remove('active');
         }
       });
+
+      const arrow = document.querySelectorAll('.arrow');
+
+      arrow.forEach((arrow) => arrow.classList.toggle('arrow-down'));
     });
   });
 });
